acc9a0d1ee84f3d978381e4c00064423b4d64a8f
nison79 <georgenison@gmail.com>
2021-03-24 01:08:12 +0200

    layout of PostBoxes

--- gatsby-config.js
+++ gatsby-config.js
@@ -58,7 +58,7 @@ module.exports = {
             {
               resolve: `gatsby-remark-images`,
               options: {
-                maxWidth: 800,
+                maxWidth: 1000,
               }
             }
           ]
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,6 @@ import { StaticImage } from 'gatsby-plugin-image'
 import styled from 'styled-components'
 
 import Layout from "../components/layout"
-import BackPaint from '../components/BackPaint'
 import SEO from "../components/seo"
 
 const BlogPreview = styled.div`
@@ -12,7 +11,8 @@ const BlogPreview = styled.div`
   flex-direction:column;
   justify-content:center;
   align-items:center;
-  padding:2rem;
+  padding:0.1rem;
+  margin-bottom:2rem;
   min-height:100vh;
   position:relative;
   h1  {
@@ -32,32 +32,53 @@ const LinkStyled = styled(Link)`
   color:black;
 `
 
+const PostBoxWrapper = styled.div`
+  display:grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 1fr 1fr;
+
+`
+
 const PostBox1 = styled.div`
   /* border:1px solid black; */
   display:flex;
   flex-direction:row;
   justify-content:space-between;
   align-items:center;
-  padding:3rem;
+  padding:0rem 5rem;
+  margin-top:8rem;
   h1{
+    
     font-size:2rem;
   }
 `
 
-const PostBoxInner = styled.div`
+const PostBoxInner1 = styled.div`
     display:flex;
     flex-direction:column;
     align-items:center;
-    padding:0rem 3rem;
+    justify-content:center;
+    padding:0rem 5rem;
+    
+  
+`
+const PostBoxInner2 = styled.div`
+    display:flex;
+    flex-direction:column;
+    /* align-items:flex-start; */
+    /* justify-content:center; */
+    justify-self:start;
+    padding:0rem 0.1rem;
+  
 `
 
 
 const PostBox2 = styled.div`
   display:flex;
   flex-direction:row;
-  justify-content:space-between;
+  justify-content:space-evenly;
   align-items:center;
-  padding:3rem;
+  padding:1rem;
   h1{
     font-size:2rem;
   }
@@ -103,32 +124,39 @@ const IndexPage = () => {
       <StaticImage 
         src="../images/posts.svg"
         alt="post-design"
-        width={200}
+        layout="constrained"
+        width={500}
       />
+      
       {/* Photography post loop */}
-      <PostBox1>
-        <h1>Photography</h1>
-        <PostBoxInner>
-        {data.photography.edges.map(post => (
+      <PostBoxWrapper>
+        <PostBox1>
+          <h1>Photography</h1>
+          <PostBoxInner1>
+          {data.photography.edges.map(post => (
+            <main>
+              <h3>
+              <LinkStyled to={post.node.frontmatter.slug}>{post.node.frontmatter.title}
+              </LinkStyled>
+              </h3>
+              <p>{post.node.frontmatter.date}</p>
+            </main>
+        ))}
+          </PostBoxInner1>
+        </PostBox1>
+            {/* Politics post loop */}
           
+        <PostBox2>
+          <h1>Politics</h1>
+          <PostBoxInner2>
+        {data.politics.edges.map(post => (
             <h3>
             <LinkStyled to={post.node.frontmatter.slug}>{post.node.frontmatter.title}</LinkStyled>
             </h3>
-      ))}
-        </PostBoxInner>
-      </PostBox1>
-          {/* Politics post loop */}
-        
-      <PostBox2>
-        <h1>Politics</h1>
-        <PostBoxInner>
-      {data.politics.edges.map(post => (
-          <h3>
-          <LinkStyled to={post.node.frontmatter.slug}>{post.node.frontmatter.title}</LinkStyled>
-          </h3>
-      ))}
-          </PostBoxInner>
-        </PostBox2>
+        ))}
+            </PostBoxInner2>
+          </PostBox2>
+      </PostBoxWrapper>
     </BlogPreview>
   </Layout>
 
